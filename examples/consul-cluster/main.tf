b5fd8fe8db8a4537786e1c08a9a91781ab0659f0
Josh Padnick <josh.padnick@gmail.com>
2017-08-17 01:03:06 -0700

    Add a cluster of Consul clients.

--- examples/consul-cluster/main.tf
+++ examples/consul-cluster/main.tf
@@ -26,14 +26,18 @@ module "consul_servers" {
   source = "../../modules/consul-cluster"
 
   gcp_zone = "${var.gcp_zone}"
-  cluster_name = "${var.cluster_name}"
+  cluster_name = "${var.consul_server_cluster_name}"
   cluster_description = "Consul Server cluster"
   machine_type = "n1-standard-1"
   assign_public_ip_addresses = false
-  instance_group_update_strategy = "RESTART"
   source_image = "consul"
   cluster_tag_name = "${var.cluster_tag_name}"
   startup_script = "${data.template_file.startup_script_server.rendered}"
+
+  # WARNING! This update strategy will delete and re-create the entire Consul cluster when making some changes to this
+  # module. Unfortunately, Google and Terraform do not yet support a stable way of performing a rolling update. For now
+  # for production usage, set this to "NONE", and manually coordinate your Consul Server upgrades per Consul docs.
+  instance_group_update_strategy = "RESTART"
 
   # Remove this if you don't want a load balancer.
   instance_group_target_pools = ["${module.load_balancer.target_pool_url}"]
@@ -43,6 +47,45 @@ module "consul_servers" {
 # This script will configure and start Consul.
 data "template_file" "startup_script_server" {
   template = "${file("${path.module}/startup-script-server.sh")}"
+
+  vars {
+    cluster_tag_name = "${var.cluster_tag_name}"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE CONSUL CLIENT NODES
+# Note that you do not have to use the consul-cluster module to deploy your clients. We do so simply because it
+# provides a convenient way to deploy an Instance Group with the necessary configuration for running Consul as a client,
+# but feel free to deploy those clients however you choose (e.g. a single Compute Instance, a Docker cluster, etc).
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "consul_clients" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "git::git@github.com:gruntwork-io/consul-aws-blueprint.git//modules/consul-cluster?ref=v0.0.1"
+  source = "../../modules/consul-cluster"
+
+  gcp_zone = "${var.gcp_zone}"
+  cluster_name = "${var.consul_client_cluster_name}"
+  cluster_description = "Consul Clients cluster"
+  machine_type = "n1-standard-1"
+  assign_public_ip_addresses = true
+  source_image = "consul"
+  cluster_tag_name = "${var.cluster_tag_name}"
+  startup_script = "${data.template_file.startup_script_client.rendered}"
+
+  # Our Consul Clients are completely stateless, so we are free to destroy and re-create them as needed.
+  instance_group_update_strategy = "RESTART"
+
+  # Remove this if you don't want a load balancer.
+  instance_group_target_pools = ["${module.load_balancer.target_pool_url}"]
+}
+
+# Render the Startup Script that will run on each Consul Server Instance on boot.
+# This script will configure and start Consul.
+data "template_file" "startup_script_client" {
+  template = "${file("${path.module}/startup-script-client.sh")}"
 
   vars {
     cluster_tag_name = "${var.cluster_tag_name}"
@@ -61,7 +104,7 @@ module "load_balancer" {
   # source = "git::git@github.com:gruntwork-io/consul-aws-blueprint.git//modules/consul-regional-load-balancer?ref=v0.0.1"
   source = "../../modules/consul-external-regional-load-balancer"
 
-  cluster_name = "${var.cluster_name}"
+  cluster_name = "${var.consul_server_cluster_name}"
   cluster_tag_name = "${var.cluster_tag_name}"
   compute_instance_group_name = "${module.consul_servers.instance_group_name}"
 }
--- examples/consul-cluster/vars.tf
+++ examples/consul-cluster/vars.tf
@@ -13,14 +13,19 @@ variable "gcp_zone" {
   default = "us-west1-a"
 }
 
-variable "cluster_name" {
+variable "consul_server_cluster_name" {
   description = "The name of the Cluster. All resources will be namespaced by this value. E.g. consul-server-prod"
   default = "consul-server-josh"
 }
 
+variable "consul_client_cluster_name" {
+  description = "The name of the Cluster. All resources will be namespaced by this value. E.g. consul-server-prod"
+  default = "consul-client-josh"
+}
+
 variable "cluster_tag_name" {
   description = "The tag the Compute Instances will look for to automatically discover each other and form a cluster. TIP: If running more than one Consul Server cluster, each cluster should have its own unique tag name. If you're not sure what to put for this value, just use the value entered in var.cluster_name."
-  default = "consul-server-josh"
+  default = "consul-josh"
 }
 
 //variable "gcp_zone" {
