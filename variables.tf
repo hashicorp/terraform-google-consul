762e7467c0242b54088c123e5ecca43610665504
Etiene Dalcol <dalcol@etiene.net>
2019-06-26 11:37:57 +0300

    minor refactor

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 variable "gcp_project_id" {
-  type        = string
   description = "The name of the GCP Project where all resources will be launched."
+  type        = string
 }
 
 variable "gcp_region" {
