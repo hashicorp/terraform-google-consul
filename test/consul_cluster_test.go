fcd97f48f4867e7f55c7d95f2a6d23814ce7788c
Rob Morgan <robbym@gmail.com>
2020-09-13 20:32:53 +0800

    add convience skip vars

--- test/consul_cluster_test.go
+++ test/consul_cluster_test.go
@@ -7,5 +7,16 @@ import (
 // Test the example in the root folder for Ubuntu 18.04
 func TestConsulClusterWithUbuntu18Image(t *testing.T) {
 	t.Parallel()
+
+	// Uncomment any of the following to skip that section during the test
+	//os.Setenv("SKIP_setup_image", "true")
+	//os.Setenv("SKIP_deploy", "true")
+	//os.Setenv("SKIP_validate", "true")
+	//os.Setenv("SKIP_build_2nd_image", "true")
+	//os.Setenv("SKIP_redeploy_cluster", "true")
+	//os.Setenv("SKIP_validate_key_exists", "true")
+	//os.Setenv("SKIP_teardown", "true")
+	//os.Setenv("SKIP_cleanup_images", "true")
+
 	runConsulClusterTest(t, "ubuntu-18-image", ".", "../examples/consul-image/consul.json")
 }
